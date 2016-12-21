64dacafcbc789ec97958402c997fdb06178515f4
jce <jce@yandex.ru>
2016-12-21 11:41:49 +0500

    Добавлены модели и админка для них

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
 
-# Register your models here.
+from django.contrib import admin
+from .models import Photo, Tag, Like
+
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+	list_display = ('id', 'user', 'created_datetime')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+	list_display = ('name', 'exclude')
+	search_fields = ('name',)
+	list_filter = ('exclude',)
+	list_editable = ('exclude',)
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+	list_display = ('user', 'photo')
--- photos/models.py
+++ photos/models.py
@@ -1,5 +1,26 @@
+# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+	name = models.CharField('Tag name', max_length=25, unique=True)
+	exclude = models.BooleanField('Exclude', default=False)
+
+
+class Photo(models.Model):
+	user = models.ForeignKey(User)
+	url = models.CharField('Url', max_length=255, unique=True)
+	created_datetime = models.DateTimeField(auto_now_add=True)
+	tags = models.ManyToManyField(Tag)
+
+	def __unicode__(self):
+		return '%s (%s)' % (self.pk, self.user)
+
+
+class Like(models.Model):
+	user = models.ForeignKey(User)
+	photo = models.ForeignKey(Photo)
