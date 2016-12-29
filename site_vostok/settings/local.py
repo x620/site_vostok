b54f3e4a5050ff0e47b1f34fe4ca77f89c50c568
jce <jce@yandex.ru>
2016-12-29 12:38:48 +0500

    Добавлен django-debug-toolbar

--- site_vostok/settings/local.py
+++ site_vostok/settings/local.py
@@ -2,6 +2,7 @@
 
 import os
 from site_vostok.settings import BASE_DIR
+from .base import INSTALLED_APPS, MIDDLEWARE
 
 DEBUG = True
 ALLOWED_HOSTS = ['*']
@@ -9,10 +10,41 @@ ALLOWED_HOSTS = ['*']
 ################################################################################
 # Database
 ################################################################################
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+################################################################################
+# Debug Toolbar
+################################################################################
+
+INTERNAL_IPS = ['127.0.0.1']
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+]
+
 ################################################################################
--- site_vostok/urls.py
+++ site_vostok/urls.py
@@ -2,8 +2,15 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from site_vostok import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('photos.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
